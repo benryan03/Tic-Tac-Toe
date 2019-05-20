5901bd4836c47923495ac652c277dfe2598b3d75
benryan03 <50218096+benryan03@users.noreply.github.com>
2019-05-20 16:53:35 -0400

    Add files via upload

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -204,26 +204,53 @@ def win_check():
 		print("O wins")
 		gameover = True	
 		
-canvas = tkinter.Canvas(root)										#defines "canvas" variable, including location(inside root)
-canvas.pack()														#activates canvas variale using "pack" placement method
+canvas = tkinter.Canvas(root, width=318, height=358)										#defines "canvas" variable, including location(inside root)
+canvas.pack()														#activates canvas variable using "pack" placement method
 
-button1 = tkinter.Button(canvas, image=blank, width=100, height=100, command=lambda: click(button1_status, 1))
+frame1 = tkinter.Frame(canvas, bg="red",)
+frame1.place(width=318, height=40)
+
+frame2 = tkinter.Frame(canvas, bg="blue",)
+frame2.place(y=40, width=318, height=318)
+
+label = tkinter.Label(frame1, text="test", bg="yellow")
+label.pack(side="left")
+
+
+
+#dropdown_default = StringVar(root)
+#variable.set("test1") # default value
+
+dropdown = tkinter.OptionMenu(frame1, "a", "test", "one", "two", "three")
+dropdown.place(relx=.5, rely=.5, width=100, height=30)
+
+
+
+
+
+
+
+
+
+
+
+button1 = tkinter.Button(frame2, image=blank, width=100, height=100, command=lambda: click(button1_status, 1))
 button1.grid(row=0, column=0)
-button2 = tkinter.Button(canvas, image=blank, width=100, height=100, command=lambda: click(button2_status, 2))
+button2 = tkinter.Button(frame2, image=blank, width=100, height=100, command=lambda: click(button2_status, 2))
 button2.grid(row=0, column=1)
-button3 = tkinter.Button(canvas, image=blank, width=100, height=100, command=lambda: click(button3_status, 3))
+button3 = tkinter.Button(frame2, image=blank, width=100, height=100, command=lambda: click(button3_status, 3))
 button3.grid(row=0, column=2)
-button4 = tkinter.Button(canvas, image=blank, width=100, height=100, command=lambda: click(button4_status, 4))
+button4 = tkinter.Button(frame2, image=blank, width=100, height=100, command=lambda: click(button4_status, 4))
 button4.grid(row=1, column=0)
-button5 = tkinter.Button(canvas, image=blank, width=100, height=100, command=lambda: click(button5_status, 5))
+button5 = tkinter.Button(frame2, image=blank, width=100, height=100, command=lambda: click(button5_status, 5))
 button5.grid(row=1, column=1)
-button6 = tkinter.Button(canvas, image=blank, width=100, height=100, command=lambda: click(button6_status, 6))
+button6 = tkinter.Button(frame2, image=blank, width=100, height=100, command=lambda: click(button6_status, 6))
 button6.grid(row=1, column=2)
-button7 = tkinter.Button(canvas, image=blank, width=100, height=100, command=lambda: click(button7_status, 7))
+button7 = tkinter.Button(frame2, image=blank, width=100, height=100, command=lambda: click(button7_status, 7))
 button7.grid(row=2, column=0)
-button8 = tkinter.Button(canvas, image=blank, width=100, height=100, command=lambda: click(button8_status, 8))
+button8 = tkinter.Button(frame2, image=blank, width=100, height=100, command=lambda: click(button8_status, 8))
 button8.grid(row=2, column=1)
-button9 = tkinter.Button(canvas, image=blank, width=100, height=100, command=lambda: click(button9_status, 9))
+button9 = tkinter.Button(frame2, image=blank, width=100, height=100, command=lambda: click(button9_status, 9))
 button9.grid(row=2, column=2)
 
 root.mainloop()															#tells root variable (base window) to remain on screen
